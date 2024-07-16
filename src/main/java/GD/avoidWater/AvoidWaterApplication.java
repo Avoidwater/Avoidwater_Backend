893d536d5cc0771cc8901eb2d82bc02729960ec6
bestdevmgp <pmg3858@icloud.com>
2024-07-16 19:35:46 +0900

    first commit

---
+++ src/main/java/GD/avoidWater/AvoidWaterApplication.java
@@ -0,0 +1,13 @@
+package GD.avoidWater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvoidWaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvoidWaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GD/avoidWater/AvoidWaterApplicationTests.java
@@ -0,0 +1,13 @@
+package GD.avoidWater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvoidWaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
