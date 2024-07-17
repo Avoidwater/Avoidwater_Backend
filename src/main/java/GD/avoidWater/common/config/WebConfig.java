f427c9b4121cb7b35e591963829ffe5a59fde48c
bestdevmgp <pmg3858@icloud.com>
2024-07-18 03:34:03 +0900

    fix bugs

--- src/main/java/GD/avoidWater/common/config/WebConfig.java
+++ src/main/java/GD/avoidWater/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081") // 허용할 출처
+                .allowedOrigins("*") // 허용할 출처
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
                 .allowCredentials(true)
                 .maxAge(3000);
